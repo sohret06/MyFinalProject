484950710de37cb49f0462eeb89771dc672b6d75
sohret06 <121291064+sohret06@users.noreply.github.com>
2024-08-02 20:02:56 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kodları
+            //Yetkilisi var mı?
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,70 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public  class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            //VERİTABANI VARMIŞ GİBİ SİMÜLE EDİYORUZ
+            _products = new List<Product>
+            {
+                new Product{CategoryId = 1 , ProductId = 1,
+                    ProductName = "Bardak", UnitPrice = 15,
+                    UnitsInStock = 15,
+                },
+                new Product{CategoryId = 1 , ProductId = 2,
+                    ProductName = "Kamera", UnitPrice = 500,
+                    UnitsInStock = 3,
+                },
+                new Product{CategoryId = 2 , ProductId = 3,
+                    ProductName = "Laptop", UnitPrice = 3500,
+                    UnitsInStock = 8,
+                },
+                new Product{CategoryId = 2 , ProductId = 4,
+                    ProductName = "Klavye", UnitPrice = 500,
+                    UnitsInStock = 8,
+                },
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId == product.ProductId);//LINQ
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+        }
+    }
+}
